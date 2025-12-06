b84a1a8139922c3cf68a5a58d3e5b8418cbcb166
trinhgsm <trinhgsm@gmail.com>
2025-12-06 12:38:07 +0700

    Update app.js

--- ban-ve/app.js
+++ ban-ve/app.js
@@ -1,7 +1,7 @@
 /**************************************************
  * CẤU HÌNH API
  **************************************************/
-const API_BASE = 'https://script.google.com/macros/s/XXXX/exec'; // ĐỔI THÀNH URL CỦA BẠN
+const API_BASE = 'https://script.google.com/macros/s/AKfycbyuCvlOdD9As5qoEwezo4cVfLn-bibmIDMD_cBvCxBIGVk54iob4nfE8smDypyj3vde/exec'; // ĐỔI THÀNH URL CỦA BẠN
 
 /**************************************************
  * STATE
