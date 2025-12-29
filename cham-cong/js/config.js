5f85b11ba7f46f860f194241f34dadad53710f01
trinhgsm <trinhgsm@gmail.com>
2025-12-29 19:16:02 +0700

    Update config.js

--- cham-cong/js/config.js
+++ cham-cong/js/config.js
@@ -1,5 +1,5 @@
 /* ===== CONFIG ===== */
-const API_URL = "https://script.google.com/macros/s/AKfycbzZRE8_2j2Tgk5w0QRHmAC2ertSS1G4jeG8U0KoYyo6KdaabekkW7GITgwv9uDaF9X5/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzIAc6J2sYYj5GRmdGGAAVvXewyuwHVMQHMk_5kiCKaDU37MjNzu643FGOZDp80Q0oBEw/exec";
 const PASSWORD = "123";
 const AUTH_KEY = "dukico_viewer_auth_v1";
 const BASE_WIDTH = 1200;
