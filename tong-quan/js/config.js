c0b6029083fa3508e651ce4a7ff5756bb106d274
trinhgsm <trinhgsm@gmail.com>
2025-12-30 19:46:35 +0700

    Create config.js

---
+++ tong-quan/js/config.js
@@ -0,0 +1,5 @@
+/* ===== CONFIG ===== */
+const API_URL = "https://script.google.com/macros/s/AKfycbzIAc6J2sYYj5GRmdGGAAVvXewyuwHVMQHMk_5kiCKaDU37MjNzu643FGOZDp80Q0oBEw/exec";
+const PASSWORD = "123";
+const AUTH_KEY = "dukico_viewer_auth_v1";
+const BASE_WIDTH = 1200;
